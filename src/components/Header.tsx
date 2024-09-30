008c31f06033b71b2be61fedc3a02a7813d1fc34
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-09-30 16:22:56 +0100

    added a bunch of components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1 className="mb-4 text-4xl bg-red-600">Exchange Kings</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
